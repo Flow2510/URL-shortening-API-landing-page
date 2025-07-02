9f8d2af2796a08ab436f4a2fd5cd837433d731f6
Flow2510 <sendra.florian@gmail.com>
2025-07-02 14:28:29 +0200

    preparation de la section results

---
+++ script.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('.shorten__input');
+const shortenButton = document.querySelector('.shorten__button');
+
+
+shortenButton.addEventListener('click', () => {
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// rentrer un lien dans l'input
+
+// envoyer le lien a l'API
+
+// creer un nouveau container avec le lien de l'input en titre, 
+
+// le shorted link en dessous
+
+// et un bouton pour copié le lien genere
